44c2c00893faffb55151160b8fd9051a1966f771
hollyana <hollyanadixon@gmail.com>
2021-11-09 00:00:35 +1300

    comments added
    table issues fixed
    new javascript code
    code commenting

--- booksjs.js
+++ booksjs.js
@@ -1,24 +1,25 @@
+// BookTok website JavaScript file
+
+// searching function
 function bookFunction() {
-  // Declare starting variables
-  var input = document.getElementById("bookInput");
-  var filter = input.value.toUpperCase();
-  var table = document.getElementById("bookTable");
-  var trs = table.tBodies[0].getElementsByTagName("tr");
-  // Loop through rows
-  for (var i = 0; i < trs.length; i++) {
-    // Define the cells
-    var tds = trs[i].getElementsByTagName("td");
-    // hide the row
-    trs[i].style.display = "none";
-    // loop through row cells
-    for (var i2 = 0; i2 < tds.length; i2++) {
-      // if there's a match
-      if (tds[i2].innerHTML.toUpperCase().indexOf(filter) > -1) {
-        // show the row
-        trs[i].style.display = "";
-        // skip to the next row
-        continue;
-      }
+    // declare variables
+    var input, filter, table, tr, td, i, ii;
+    input = document.getElementById("bookInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("bookTable");
+    tr = table.querySelectorAll("tbody tr:not(.header)");
+
+    // loop through rows and cells and hide things that don't match the search
+    for (i = 0; i < tr.length; i++) {
+        var tds = tr[i].getElementsByTagName("td");
+        var found = false;
+        for (ii = 0; ii < tds.length && !found; ii++) {
+            if (tds[ii].textContent.toUpperCase().indexOf(filter) > -1) {
+                found = true;
+                break;
+            }
+        }
+        // show found rows
+        tr[i].style.display = found?"":"none";
     }
-  }
 }
